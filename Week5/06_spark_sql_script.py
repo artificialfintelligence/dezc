367524676d87174c99811fcb251f9f28c3f105a5
Farid Tabatabaie <farid.t@gmail.com>
2023-10-31 14:29:57 -0700

    Replace deprecated method

--- Week5/06_spark_sql_script.py
+++ Week5/06_spark_sql_script.py
@@ -10,9 +10,9 @@ from pyspark.sql import functions as F
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--input_green', required=True)
-parser.add_argument('--input_yellow', required=True)
-parser.add_argument('--output', required=True)
+parser.add_argument("--input_green", required=True)
+parser.add_argument("--input_yellow", required=True)
+parser.add_argument("--output", required=True)
 
 args = parser.parse_args()
 
@@ -22,68 +22,68 @@ output = args.output
 
 
 spark = SparkSession.builder \
-    .appName('test') \
+    .appName("test") \
     .getOrCreate()
 
 df_green = spark.read.parquet(input_green)
 
 df_green = df_green \
-    .withColumnRenamed('lpep_pickup_datetime', 'pickup_datetime') \
-    .withColumnRenamed('lpep_dropoff_datetime', 'dropoff_datetime')
+    .withColumnRenamed("lpep_pickup_datetime", "pickup_datetime") \
+    .withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
 
 df_yellow = spark.read.parquet(input_yellow)
 
 
 df_yellow = df_yellow \
-    .withColumnRenamed('tpep_pickup_datetime', 'pickup_datetime') \
-    .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')
+    .withColumnRenamed("tpep_pickup_datetime", "pickup_datetime") \
+    .withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
 
 
 common_colums = [
-    'VendorID',
-    'pickup_datetime',
-    'dropoff_datetime',
-    'store_and_fwd_flag',
-    'RatecodeID',
-    'PULocationID',
-    'DOLocationID',
-    'passenger_count',
-    'trip_distance',
-    'fare_amount',
-    'extra',
-    'mta_tax',
-    'tip_amount',
-    'tolls_amount',
-    'improvement_surcharge',
-    'total_amount',
-    'payment_type',
-    'congestion_surcharge'
+    "VendorID",
+    "pickup_datetime",
+    "dropoff_datetime",
+    "store_and_fwd_flag",
+    "RatecodeID",
+    "PULocationID",
+    "DOLocationID",
+    "passenger_count",
+    "trip_distance",
+    "fare_amount",
+    "extra",
+    "mta_tax",
+    "tip_amount",
+    "tolls_amount",
+    "improvement_surcharge",
+    "total_amount",
+    "payment_type",
+    "congestion_surcharge"
 ]
 
 
 
 df_green_sel = df_green \
     .select(common_colums) \
-    .withColumn('service_type', F.lit('green'))
+    .withColumn("service_type", F.lit("green"))
 
 df_yellow_sel = df_yellow \
     .select(common_colums) \
-    .withColumn('service_type', F.lit('yellow'))
+    .withColumn("service_type", F.lit("yellow"))
 
 
 df_trips_data = df_green_sel.unionAll(df_yellow_sel)
 
-df_trips_data.registerTempTable('trips_data')
+df_trips_data.createOrReplaceTempView("trips_data")
 
 
 df_result = spark.sql("""
-SELECT 
-    -- Reveneue grouping 
+SELECT
+    -- Reveneue grouping
     PULocationID AS revenue_zone,
-    date_trunc('month', pickup_datetime) AS revenue_month, 
-    service_type, 
+    date_trunc("month", pickup_datetime) AS revenue_month,
+    service_type,
 
-    -- Revenue calculation 
+    -- Revenue calculation
     SUM(fare_amount) AS revenue_monthly_fare,
     SUM(extra) AS revenue_monthly_extra,
     SUM(mta_tax) AS revenue_monthly_mta_tax,
@@ -104,8 +104,4 @@ GROUP BY
 
 
 df_result.coalesce(1) \
-    .write.parquet(output, mode='overwrite')
-
-
-
-
+    .write.parquet(output, mode="overwrite")
